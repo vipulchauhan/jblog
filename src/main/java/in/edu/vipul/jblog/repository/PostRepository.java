b3859c246304510c9de1c8e1bdaaaf33af2f51ee
vipul <08bce016@gmail.com>
2018-11-25 17:47:52 +0530

    added post entity

---
+++ src/main/java/in/edu/vipul/jblog/repository/PostRepository.java
@@ -0,0 +1,15 @@
+package in.edu.vipul.jblog.repository;
+
+import in.edu.vipul.jblog.domain.Post;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Post entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
